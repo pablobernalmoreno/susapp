021b9eda5df99f0bd310c67ff85da11cbb1fd022
pablobernalmoreno <pbm1998@hotmail.com>
2025-05-24 10:55:57 -0500

    pagination logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,159 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Typography,
+  Pagination,
+  Paper,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  Button,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+
+const questions = [
+  {
+    id: 0,
+    question: "Question 1",
+    options: [
+      {
+        id: 0,
+        label: "A",
+        type: "correct",
+        reason: "Because it is A",
+      },
+      {
+        id: 1,
+        label: "B",
+        type: "incorrect",
+        reason: "Because it is not A",
+      },
+      {
+        id: 2,
+        label: "C",
+        type: "incorrect",
+        reason: "Because it is not A",
+      },
+      {
+        id: 3,
+        label: "D",
+        type: "incorrect",
+        reason: "Because it is not A",
+      },
+    ],
+  },
+  {
+    id: 1,
+    question: "Question 2",
+    options: [
+      {
+        id: 0,
+        label: "A",
+        type: "incorrect",
+        reason: "Because it is not B",
+      },
+      {
+        id: 1,
+        label: "B",
+        type: "correct",
+        reason: "Because it is B",
+      },
+      {
+        id: 2,
+        label: "C",
+        type: "incorrect",
+        reason: "Because it is not B",
+      },
+      {
+        id: 3,
+        label: "D",
+        type: "incorrect",
+        reason: "Because it is not B",
+      },
+    ],
+  },
+];
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [page, setPage] = useState(1);
+  const [selectedQuestion, setSelectedQuestion] = useState(questions[0]);
+  const [value, setValue] = useState("");
+  const [selectedOption, setSelectedOption] = useState("");
+  const [expanded, setExpanded] = useState(false);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const handleOptionsChange = (event) => {
+    const selected = selectedQuestion?.options?.find(
+      (option) => option?.label === event?.target?.value
+    );
+    setValue(event?.target?.value);
+    setSelectedOption(selected);
+  };
+
+  const handlePaginationChange = (event, value) => {
+    setPage(value);
+    setSelectedQuestion(questions[value - 1]);
+    setValue("");
+    setSelectedOption("");
+  };
+
+  const handleAccordionExpand = () => {
+    setExpanded((expanded) => !expanded);
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Paper
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "2rem",
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          {selectedQuestion?.question}
+        </Typography>
+        <FormControl>
+          <RadioGroup value={value} onChange={handleOptionsChange}>
+            {selectedQuestion?.options?.map((option) => {
+              return (
+                <FormControlLabel
+                  key={option?.id}
+                  value={option?.label}
+                  control={<Radio />}
+                  label={option?.label}
+                />
+              );
+            })}
+          </RadioGroup>
+        </FormControl>
+        <Accordion expanded={expanded} onChange={handleAccordionExpand}>
+          <AccordionSummary>
+            <Typography component="span">Check Answer</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>{selectedOption?.reason}</Typography>
+          </AccordionDetails>
+        </Accordion>
+        <Pagination
+          count={questions?.length}
+          page={page}
+          onChange={handlePaginationChange}
+        />
+      </Paper>
+    </Box>
   );
 }
